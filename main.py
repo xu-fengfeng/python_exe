baa224d0f7f8d08bbbafd1cb3b954dea65f4b522
xufengfeng <xu-fengfeng@msn.com>
2015-11-17 22:05:00 +0800

    by xufengfeng
    add a reverse function，
    eg：
    input 123455
    output 554321

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 from PyQt5 import QtWidgets,QtCore,QtGui
 
@@ -11,14 +12,44 @@ class MainWindow(QtWidgets.QWidget):
         OutputLabel = QtWidgets.QLabel("Output:")
         self.OutputLine =QtWidgets.QLineEdit()
 
+        self.ConfirmButton = QtWidgets.QPushButton("&Reverse")
+        self.ConfirmButton.clicked.connect(self.valueChange)
+        self.ConfirmButton.show()
+        
+        buttonLayout1 = QtWidgets.QVBoxLayout()
+        buttonLayout1.addWidget(self.ConfirmButton, QtCore.Qt.AlignTop)
+        buttonLayout1.addStretch()        
+        
+        #mainLayout是主界面，addWidget把item添加到主界面
         mainLayout = QtWidgets.QGridLayout()
         mainLayout.addWidget(InputLabel, 0, 0)
         mainLayout.addWidget(self.InputLine, 0, 1)
         mainLayout.addWidget(OutputLabel, 1, 0, QtCore.Qt.AlignTop)
         mainLayout.addWidget(self.OutputLine, 1, 1)
+        
+        mainLayout.addLayout(buttonLayout1, 2, 1)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("Ex1:Reverse string")
+        
+    def valueChange(self):
+        str = self.InputLine.text()
+        
+        if str == "":
+            print ("inputline is empyt")
+        else:
+            print ("inputline = %s"%str)
+            newstr = self.reverseStr(str)
+
+            #print ("newstr = %s"%newstr)
+            self.OutputLine.setText(newstr)
+
+    def reverseStr(self,str):
+        newlist = list(str)
+        newlist.reverse()
+        #print (newlist)
+        newstr = "".join(newlist)
+        return newstr
         
 if __name__ == '__main__':
     print ("start...")
