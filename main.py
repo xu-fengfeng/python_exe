21fa9a6620efdd3dc64386d3268ec051c26d1828
xufengfeng <xu-fengfeng@msn.com>
2015-11-20 22:05:47 +0800

    add pig-latin game

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import sys
 from PyQt5 import QtWidgets,QtCore,QtGui
 from PyQt5.uic import loadUi
 
+class LatinPigGame(object):
+    def __init__(self):
+        vowellist = "aeiuo"
+    def LatinPigChange(self,input):
+        if not isinstance(input, str):
+            print ("str is not a string")
+        return input
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -12,49 +19,17 @@ class MainWindow(QtWidgets.QMainWindow):
         
         self.ConfirmButton.clicked.connect(self.valueChange)
         self.ExitButton.clicked.connect(self.Exit)
-        
-        '''
-        InputLabel = QtWidgets.QLabel("Input:")
-        self.InputLine = QtWidgets.QLineEdit()
+        self.LatinPigButton.clicked.connect(self.latinPigGame)
 
-        OutputLabel = QtWidgets.QLabel("Output:")
-        self.OutputLine =QtWidgets.QLineEdit()
-
-        self.ConfirmButton = QtWidgets.QPushButton("&Reverse")
-        self.ConfirmButton.clicked.connect(self.valueChange)
-        self.ConfirmButton.show()
-        
-        self.ExitButton = QtWidgets.QPushButton("&Exit")
-        self.ExitButton.clicked.connect(self.Exit)
-        
-        #buttonLayout1 = QtWidgets.QVBoxLayout()
-        buttonLayout1 = QtWidgets.QHBoxLayout()
-        buttonLayout1.addWidget(self.ConfirmButton, QtCore.Qt.AlignTop)
-        buttonLayout1.addWidget(self.ExitButton, QtCore.Qt.AlignTop)
-        #buttonLayout1.addStretch()        
-        
-        #mainLayout是主界面，addWidget把item添加到主界面
-        mainLayout = QtWidgets.QGridLayout()
-        mainLayout.addWidget(InputLabel, 0, 0)
-        mainLayout.addWidget(self.InputLine, 0, 1)
-        mainLayout.addWidget(OutputLabel, 1, 0, QtCore.Qt.AlignTop)
-        mainLayout.addWidget(self.OutputLine, 1, 1)
-        
-        mainLayout.addLayout(buttonLayout1, 2, 1)
-
-        self.setLayout(mainLayout)
-        self.setWindowTitle("Ex1:Reverse string")
-    '''        
     def valueChange(self):
         str = self.InputLine.text()
         
         if str == "":
-            print ("inputline is empyt")
+            print ("inputline is empyt.")
         else:
             print ("inputline = %s"%str)
             newstr = self.reverseStr(str)
 
-            #print ("newstr = %s"%newstr)
             self.OutputLine.setText(newstr)
 
     def reverseStr(self,str):
@@ -63,9 +38,19 @@ class MainWindow(QtWidgets.QMainWindow):
         #print (newlist)
         newstr = "".join(newlist)
         return newstr
-    
+    def latinPigGame(self):
+        str = self.InputLine.text()
+        if str == "":
+            print ("inputline is empty.")
+        else:
+            print ("inputline = %s"%str)
+            lpg = LatinPigGame()
+            newstr = lpg.LatinPigChange(str)
+
+            self.OutputLine.setText(newstr)
     def Exit(self):
         QtWidgets.QApplication.quit()
+        
     
         
 if __name__ == '__main__':
