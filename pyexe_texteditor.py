35ed6d8117eb45ba7b310fe98d23418b4d8fd66b
xu-fengfeng <xu-fengfeng@msn.com>
2016-01-14 23:24:52 +0800

    create menu bar as object

--- pyexe_texteditor.py
+++ pyexe_texteditor.py
@@ -4,6 +4,53 @@ from PyQt5 import QtWidgets,QtCore,QtGui
 from PyQt5.uic import loadUi
 from _overlapped import NULL
 
+class py_filemenu():
+    def __init__(self, mainwindow):
+        self.menubar = mainwindow.menuBar()
+        self.filemenu = self.menubar.addMenu("&File")
+        
+        self.newAction = QtWidgets.QAction("&New", mainwindow, shortcut=NULL, statusTip="Create a new file", triggered=self.newFile)
+        self.filemenu.addAction(self.newAction)  
+        
+        self.openAction = QtWidgets.QAction("&Open", mainwindow, shortcut=NULL,
+                statusTip="open a file", triggered=self.openFile)
+        self.filemenu.addAction(self.openAction)
+        
+        self.closeAction = QtWidgets.QAction("&Close", mainwindow, shortcut=NULL,
+                statusTip="Close.",triggered = self.closeWindow)      
+        self.filemenu.addAction(self.closeAction)
+             
+    def newFile(self):
+        pass
+    def openFile(self):
+        pass
+    def closeWindow(self):
+        pass    
+
+class py_editmenu():
+    def __init__(self, mainwindow):
+        self.menubar = mainwindow.menuBar() 
+        
+        self.editmenu = self.menubar.addMenu("&Edit")               
+        
+        self.copyAction = QtWidgets.QAction("&Copy", mainwindow, shortcut=NULL,
+                statusTip="copy.", triggered = self.copyContent)
+        self.editmenu.addAction(self.copyAction)
+    def copyContent(self):
+        pass          
+
+class py_aboutmenu():
+    def __init__(self,mainwindow):
+        self.menubar = mainwindow.menuBar()
+        self.helpmenu = self.menubar.addMenu("&Help")   
+        self.aboutAction = QtWidgets.QAction("&About", mainwindow, shortcut=NULL,
+                statusTip="about.", triggered = self.aboutFunction)
+             
+        self.helpmenu.addAction(self.aboutAction)
+    def aboutFunction(self):
+        pass        
+
+
 class py_texteditor(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(py_texteditor, self).__init__(parent)
@@ -14,44 +61,11 @@ class py_texteditor(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon("qt-logo.png"))
         self.setGeometry(400,250,500,300)
         
-        self.createActions()
-        self.addmenubar()
-                
-    def createActions(self):
-        self.newAction = QtWidgets.QAction("&New", self, shortcut=NULL,
-                statusTip="Create a new file", triggered=self.newFile)
-        self.openAction = QtWidgets.QAction("&Open", self, shortcut=NULL,
-                statusTip="open a file", triggered=self.openFile)
-        self.closeAction = QtWidgets.QAction("&Close", self, shortcut=NULL,
-                statusTip="Close.",triggered = self.closeWindow)      
-        self.copyAction = QtWidgets.QAction("&Copy", self, shortcut=NULL,
-                statusTip="copy.", triggered = self.copyContent)
-        self.aboutAction = QtWidgets.QAction("&About", self, shortcut=NULL,
-                statusTip="copy.", triggered = self.aboutFunction)
-    def addmenubar(self):
         self.menubar = self.menuBar()
-        self.filemenu = self.menubar.addMenu("&File")
-        self.editmenu = self.menubar.addMenu("&Edit")
-        self.aboutmenu = self.menubar.addMenu("&About")        
         
-        self.filemenu.addAction(self.newAction)
-        self.filemenu.addAction(self.openAction)
-        self.filemenu.addAction(self.closeAction)
-        
-        self.editmenu.addAction(self.copyAction)
-        
-        self.aboutmenu.addAction(self.aboutAction)
-              
-    def newFile(self):
-        pass
-    def openFile(self):
-        pass
-    def closeWindow(self):
-        pass    
-    def copyContent(self):
-        pass  
-    def aboutFunction(self):
-        pass
+        py_filemenu(self)
+        py_editmenu(self)
+        py_aboutmenu(self)
         
 
 if __name__ == "__main__":
